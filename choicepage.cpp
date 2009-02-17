5825d127053f81d1145a8db778709dcae28ed34a
Sune Vuorela <sune@vuorela.dk>
2009-02-17 10:30:11 +0100

    add copyright header

--- choicepage.cpp
+++ choicepage.cpp
@@ -1,3 +1,19 @@
+/*
+    Copyright (C) 2009 Sune Vuorela <sune@vuorela.dk>
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "choicepage.h"
 
 class ChoicePagePrivate
@@ -15,8 +31,9 @@ class ChoicePagePrivate
 
 ChoicePage::ChoicePage(QWidget *parent) : QWizardPage(parent)
 {
-  QVBoxLayout *lay = new QVBoxLayout(this);
   d = new ChoicePagePrivate;
+  QVBoxLayout *lay = new QVBoxLayout(this);
+  setTitle("Kaboom - MigrationTool");
   d->haskde4dir = QFile::exists(QDir::homePath()+"/"+".kde4");
   d->buttons = new QButtonGroup(this);
   d->text = new QLabel(tr("Please select the option on how you want to migrate your settings"),this);
