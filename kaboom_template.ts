8b8b9d1bd83431a2c63ab1d6acdf01155fc93d90
Sune Vuorela <sune@vuorela.dk>
2009-02-23 22:47:16 +0100

    translation template

---
+++ kaboom_template.ts
@@ -0,0 +1,125 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>ChoicePage</name>
+    <message>
+        <location filename="choicepage.cpp" line="40"/>
+        <source>Kaboom - MigrationTool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="44"/>
+        <source>Please select the option on how you want to migrate your settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="48"/>
+        <source>Migrate settings from KDE3 to KDE4 (recommended)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="55"/>
+        <source>Move settings from KDE 4 dir and &lt;b&gt;replace&lt;/b&gt; settings from KDE 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="60"/>
+        <source>Merge settings from KDE3 and KDE4 (experimental)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="69"/>
+        <source>Start with a fresh KDE. This option will &lt;b&gt;remove&lt;/b&gt; data and settings such as contacts, local stored mails, accounts in KMail and Kopete, bookmarks and other such data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="79"/>
+        <source>You have not enough free space to actually do a backup, please consider freeing up some space by going to TTY1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="83"/>
+        <source>The current kde settings and data dir takes up %1 bytes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="90"/>
+        <source>Backup existing settings from KDE3 into .kde3-backup. (Highly recommended)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IntroPage</name>
+    <message>
+        <location filename="intropage.cpp" line="22"/>
+        <source>This wizard will guide you thru the migration of KDE 3 and KDE 4 settings as required</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="33"/>
+        <source>User canceled the operation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="34"/>
+        <source>Access was denied to the file or directory %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="35"/>
+        <source>%1: No such file or directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="36"/>
+        <source>%1 already exists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="37"/>
+        <source>Could not copy %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="38"/>
+        <source>Could not create directory %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="39"/>
+        <source>Could not remove %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="40"/>
+        <source>Unknown error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="109"/>
+        <source>Calculating... %1 bytes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="153"/>
+        <source>Copying files...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="260"/>
+        <source>Removing files...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WarningPage</name>
+    <message>
+        <location filename="warningpage.cpp" line="21"/>
+        <source>You have selected a potentially harmful operation and not to backup data. You will most likely lose data here. Are you completely sure?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
