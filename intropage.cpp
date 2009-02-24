c3dfbea8f1b1b404fa824b2c6a7f675207e2f89b
Sune Vuorela <sune@vuorela.dk>
2009-02-24 08:47:45 +0100

    wordwrap on label and proper  Qt parenthood

--- intropage.cpp
+++ intropage.cpp
@@ -19,7 +19,8 @@
 IntroPage::IntroPage(QWidget *parent) : QWizardPage(parent)
 {
   setTitle(tr("Kaboom - MigrationTool"));
-  QLabel *text = new QLabel(tr("This wizard will guide you thru the migration of KDE 3 and KDE 4 settings as required"));
+  QLabel *text = new QLabel(tr("This wizard will guide you thru the migration of KDE 3 and KDE 4 settings as required"),this);
+  text->setWordWrap(true);
   QVBoxLayout *lay = new QVBoxLayout();
   lay->addWidget(text);
   setLayout(lay);
