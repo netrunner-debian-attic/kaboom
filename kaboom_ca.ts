66a213c49a3afb080b7c53e7c7d1b6fc02b3e521
Ana Guerrero <ana@ekaia.org>
2009-02-25 00:18:39 +0100

    spanish and catalan translations

---
+++ kaboom_ca.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="ca">
+<defaultcodec></defaultcodec>
+<context>
+    <name>ChoicePage</name>
+    <message>
+        <location filename="choicepage.cpp" line="40"/>
+        <source>Kaboom - MigrationTool</source>
+        <translation>Kaboom - Eïna de migració</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="44"/>
+        <source>Please select your settings migratation option</source>
+        <translation>Per favor seleccione les seves opcions de configuració </translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="49"/>
+        <source>Migrate settings from KDE3 to KDE4 (standard)</source>
+        <translation>Mirgrar configuració de KDE3 a KDE4 (estándar)</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="56"/>
+        <source>Use existing KDE 4 settings and &lt;b&gt;replace&lt;/b&gt; KDE 3 settings</source>
+        <translation>Usar la configuraciò de KDE4 i &lt;b&gt;sobreescriure&lt;/b&gt; la de KDE3</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="61"/>
+        <source>Merge settings from KDE3 and KDE4 (experimental)</source>
+        <translation>Combinar la configuració de KDE3 i KDE4 (experimental)</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="70"/>
+        <source>Start with default KDE settings and data. This option will &lt;b&gt;remove&lt;/b&gt; all data and settings such as contacts, local stored mails, accounts in KMail and Kopete, bookmarks and other such data</source>
+        <translation>Començar amb la configuració i dades per defecte de KDE. Esta opció &lt;b&gt;eliminarà&lt;/b&gt; tota la informaciò i configuraciò, com ara els contactes, correus guardats, conters de KMail i Kopete, marcadors i altra informaciò semblant</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="73"/>
+        <source>Backup existing settings from KDE3 into .kde3-backup. (Highly recommended)</source>
+        <translation>Fer una copia de seguretat de KDE3 a la carpeta .kde3-backup. (Altament recomanat)</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="92"/>
+        <source>Insufficient free space to complete a backup, please consider freeing up some space. You can go to TTY1 to do this.</source>
+        <translation>Insuficient espai lliure per a completar una copia de seguretat. Per favor, lliure una mica d&apos;espai. Pot anar a la TTY1 per a fer-ho.</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="96"/>
+        <source>The current KDE settings and data directory takes up %1 bytes</source>
+        <translation>La configuraciò actual i la carpeta de dades de KDE ocupen %1 bytes</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="112"/>
+        <source>Recheck</source>
+        <translation>Comprovar de nou</translation>
+    </message>
+</context>
+<context>
+    <name>IntroPage</name>
+    <message>
+        <location filename="intropage.cpp" line="21"/>
+        <source>Kaboom - MigrationTool</source>
+        <translation>Kaboom - Eïna de migraciò</translation>
+    </message>
+    <message>
+        <location filename="intropage.cpp" line="22"/>
+        <source>This wizard will guide you through the process of migrating your KDE settings as required</source>
+        <translation>Aquest asistent el guiarà a través del procés de migrar la seva configuraciò de KDE</translation>
+    </message>
+</context>
+<context>
+    <name>MigrationPage</name>
+    <message>
+        <location filename="migrationpage.cpp" line="103"/>
+        <source>When you click &quot;start&quot;, settitgs migration will start</source>
+        <translation>Quan clique &quot;Iniciar&quot;, començará la migració de la configuració </translation>
+    </message>
+    <message>
+        <location filename="migrationpage.cpp" line="106"/>
+        <source>Start Migration</source>
+        <translation>Iniciar Migraciò</translation>
+    </message>
+</context>
+<context>
+    <name>MigrationPagePrivate</name>
+    <message>
+        <location filename="migrationpage.cpp" line="30"/>
+        <source>Start</source>
+        <translation>Iniciar</translation>
+    </message>
+    <message>
+        <location filename="migrationpage.cpp" line="32"/>
+        <source>Errors occured</source>
+        <translation>Errors en el procés</translation>
+    </message>
+    <message>
+        <location filename="migrationpage.cpp" line="42"/>
+        <source>Migration running</source>
+        <translation>Migraciò en marxa</translation>
+    </message>
+</context>
+<context>
+    <name>MigrationTool</name>
+    <message>
+        <location filename="migrationtool.cpp" line="37"/>
+        <source>&amp;Back</source>
+        <translation>&amp;Anterior</translation>
+    </message>
+    <message>
+        <location filename="migrationtool.cpp" line="38"/>
+        <source>&amp;Next</source>
+        <translation>&amp;Posterior</translation>
+    </message>
+    <message>
+        <location filename="migrationtool.cpp" line="39"/>
+        <source>&amp;Finish</source>
+        <translation>&amp;Finalitzar</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="33"/>
+        <source>User canceled the operation</source>
+        <translation>El usuari cancelà la operaciò</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="34"/>
+        <source>Access was denied to the file or directory %1</source>
+        <translation>El acces fou denegat per al fitxer o directori %1</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="35"/>
+        <source>%1: No such file or directory</source>
+        <translation>%1: Fitxer o directori </translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="36"/>
+        <source>%1 already exists</source>
+        <translation>%1 ja existeix</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="37"/>
+        <source>Could not copy %1</source>
+        <translation>No es pot copiar %1</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="38"/>
+        <source>Could not create directory %1</source>
+        <translation>No es pot crear el directori %1</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="39"/>
+        <source>Could not remove %1</source>
+        <translation>No es pot eliminar %1</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="40"/>
+        <source>Unknown error</source>
+        <translation>Error desconegut</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="109"/>
+        <source>Calculating... %1 bytes</source>
+        <translation>Calculant... %1 bytes</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="153"/>
+        <source>Copying files...</source>
+        <translation>Copiant fitxers...</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="260"/>
+        <source>Removing files...</source>
+        <translation>Borrant fitxers...</translation>
+    </message>
+</context>
+<context>
+    <name>WarningPage</name>
+    <message>
+        <location filename="warningpage.cpp" line="21"/>
+        <source>Warning</source>
+        <translation>Atenciò</translation>
+    </message>
+    <message>
+        <location filename="warningpage.cpp" line="22"/>
+        <source>You have selected a potentially harmful operation and to not backup data. You will most likely lose data here. Are you completely sure?</source>
+        <translation>Ha seleccionat una operació potencialment danyina i no guardar copia de les dades. Amb molta probabilitat perdrà dades en aquest procés. Està completament segur?</translation>
+    </message>
+</context>
+</TS>
---
+++ kaboom_es.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="es">
+<defaultcodec></defaultcodec>
+<context>
+    <name>ChoicePage</name>
+    <message>
+        <location filename="choicepage.cpp" line="40"/>
+        <source>Kaboom - Migration Tool</source>
+        <oldsource>Kaboom - MigrationTool</oldsource>
+        <translation type="unfinished">Kaboom - Utilidad de migración</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="44"/>
+        <source>Please select your settings migration option</source>
+        <oldsource>Please select your settings migratation option</oldsource>
+        <translation type="unfinished">Seleccione sus opciones de configuración de migración</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="49"/>
+        <source>Migrate settings from KDE3 to KDE4 (standard)</source>
+        <translation type="unfinished">Migrar las configuraciones de KDE 3 a KDE 4 (estándar)</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="56"/>
+        <source>Use existing KDE 4 settings and &lt;b&gt;replace&lt;/b&gt; KDE 3 settings</source>
+        <translation type="unfinished">Usar las configuraciones actuales de KDE 4 y &lt;b&gt;reemplazar&lt;/b&gt; las configuraciones de KDE 3</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="61"/>
+        <source>Merge settings from KDE3 and KDE4 (experimental)</source>
+        <translation type="unfinished">Fusionar las configuraciones de KDE 3 y KDE 4 (experimental)</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="70"/>
+        <source>Start with default KDE settings and data. This option will &lt;b&gt;remove&lt;/b&gt; all data and settings such as contacts, local stored mails, accounts in KMail and Kopete, bookmarks and other such data</source>
+        <translation type="unfinished">Empezar con las configuraciones y datos por defecto de KDE . Esta opción &lt;b&gt;eliminará&lt;/b&gt; todos los datos y configuraciones tales como contactos, correos almacenados, cuentas en KMail y Kopete, marcadores y otros datos por el estilo</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="73"/>
+        <source>Backup existing settings from KDE3 into .kde3-backup. (Highly recommended)</source>
+        <translation type="unfinished">Crear una copia de seguridad de la configuración de KDE3 en .kde3-backup. (Opción altamente recomendada)</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="92"/>
+        <source>Insufficient free space to complete a backup, please consider freeing up some space. You can go to TTY1 to do this.</source>
+        <translation type="unfinished">No hay suficiente espacio libre para crear una copia de seguridad, considere liberar algo de espacio en su disco. Puede ir a TTY1 para hacer esto.</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="96"/>
+        <source>The current KDE settings and data directory takes up %1 bytes</source>
+        <translation type="unfinished">Los datos y configuraciones actuales de KDE ocupan %1 bytes</translation>
+    </message>
+    <message>
+        <location filename="choicepage.cpp" line="112"/>
+        <source>Recheck</source>
+        <translation type="unfinished">Recomprobar</translation>
+    </message>
+</context>
+<context>
+    <name>IntroPage</name>
+    <message>
+        <location filename="intropage.cpp" line="21"/>
+        <source>Kaboom - Migration Tool</source>
+        <oldsource>Kaboom - MigrationTool</oldsource>
+        <translation type="unfinished">Kaboom - Utilidad de migración</translation>
+    </message>
+    <message>
+        <location filename="intropage.cpp" line="22"/>
+        <source>This wizard will guide you through the process of migrating your KDE settings as required</source>
+        <translation type="unfinished">Este asistente le guiará a través del proceso de migración de las configuraciones de KDE</translation>
+    </message>
+</context>
+<context>
+    <name>MigrationPage</name>
+    <message>
+        <location filename="migrationpage.cpp" line="103"/>
+        <source>When you click &quot;Start&quot;, settings migration will start</source>
+        <oldsource>When you click &quot;start&quot;, settitgs migration will start</oldsource>
+        <translation type="unfinished">Cuando seleccione &quot;Comenzar&quot;, se comenzará la migración de configuraciones</translation>
+    </message>
+    <message>
+        <location filename="migrationpage.cpp" line="106"/>
+        <source>Start Migration</source>
+        <translation type="unfinished">Comenzar migración</translation>
+    </message>
+</context>
+<context>
+    <name>MigrationPagePrivate</name>
+    <message>
+        <location filename="migrationpage.cpp" line="30"/>
+        <source>Start</source>
+        <translation type="unfinished">Comenzar</translation>
+    </message>
+    <message>
+        <location filename="migrationpage.cpp" line="32"/>
+        <source>Errors occured</source>
+        <translation type="unfinished">Se encontró un problema</translation>
+    </message>
+    <message>
+        <location filename="migrationpage.cpp" line="42"/>
+        <source>Migration running</source>
+        <translation type="unfinished">Migración ejecutándose</translation>
+    </message>
+</context>
+<context>
+    <name>MigrationTool</name>
+    <message>
+        <location filename="migrationtool.cpp" line="37"/>
+        <source>&amp;Back</source>
+        <translation type="unfinished">&amp;Anterior</translation>
+    </message>
+    <message>
+        <location filename="migrationtool.cpp" line="38"/>
+        <source>&amp;Next</source>
+        <translation type="unfinished">&amp;Siguiente</translation>
+    </message>
+    <message>
+        <location filename="migrationtool.cpp" line="39"/>
+        <source>&amp;Finish</source>
+        <translation type="unfinished">&amp;Finalizar</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="33"/>
+        <source>User canceled the operation</source>
+        <translation type="unfinished">Operación cancelada por el usuario</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="34"/>
+        <source>Access was denied to the file or directory %1</source>
+        <translation type="unfinished">Le fue denegado el acceso al fichero o directorio %1</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="35"/>
+        <source>%1: No such file or directory</source>
+        <translation type="unfinished">%1: No existe ese fichero o directorio</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="36"/>
+        <source>%1 already exists</source>
+        <translation type="unfinished">%1 ya existe</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="37"/>
+        <source>Could not copy %1</source>
+        <translation type="unfinished">No se pudo copiar %1</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="38"/>
+        <source>Could not create directory %1</source>
+        <translation type="unfinished">No se pudo crear el directorio %1</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="39"/>
+        <source>Could not remove %1</source>
+        <translation type="unfinished">No se pudo eliminar %1</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="40"/>
+        <source>Unknown error</source>
+        <translation type="unfinished">Error desconocido</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="109"/>
+        <source>Calculating... %1 bytes</source>
+        <translation type="unfinished">Calculando... %1 bytes</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="153"/>
+        <source>Copying files...</source>
+        <translation type="unfinished">Copiando ficheros...</translation>
+    </message>
+    <message>
+        <location filename="diroperations/diroperations.cpp" line="260"/>
+        <source>Removing files...</source>
+        <translation type="unfinished">Eliminando ficheros...</translation>
+    </message>
+</context>
+<context>
+    <name>WarningPage</name>
+    <message>
+        <location filename="warningpage.cpp" line="21"/>
+        <source>Warning</source>
+        <translation type="unfinished">Atención</translation>
+    </message>
+    <message>
+        <location filename="warningpage.cpp" line="22"/>
+        <source>You have selected a potentially harmful operation and to not backup data. You will most likely lose data here. Are you completely sure?</source>
+        <translation type="unfinished">Ha seleccionado una operación potencialmente dañina y no crear una copia de seguridad de los datos. Es muy posible que pierda datos. ¿Está completamente seguro?</translation>
+    </message>
+</context>
+</TS>
