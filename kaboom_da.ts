510dd3022f1ec6638d98321ab6478c716adbfeed
Sune Vuorela <sune@vuorela.dk>
2009-02-24 23:39:20 +0100

    translations updates

--- kaboom_da.ts
+++ kaboom_da.ts
@@ -1,56 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="1.1" language="da">
+<defaultcodec></defaultcodec>
 <context>
     <name>ChoicePage</name>
     <message>
         <location filename="choicepage.cpp" line="40"/>
         <source>Kaboom - MigrationTool</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaboom - Migreringsværktøj</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="44"/>
         <source>Please select your settings migratation option</source>
-        <translation type="unfinished"></translation>
+        <translation>Vælg migreringsmuligheder</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="49"/>
         <source>Migrate settings from KDE3 to KDE4 (standard)</source>
-        <translation type="unfinished"></translation>
+        <translation>Migrer indstillinger fra KDE 3 til KDE 4 (standard)</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="56"/>
         <source>Use existing KDE 4 settings and &lt;b&gt;replace&lt;/b&gt; KDE 3 settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Brug eksisterende KDE 4 indstillinger og &lt;b&gt;erstat&lt;/b&gt; KDE 3 indstillinger</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="61"/>
         <source>Merge settings from KDE3 and KDE4 (experimental)</source>
-        <translation type="unfinished"></translation>
+        <translation>Flet indstillinger fra KDE3 og KDE4 (experimentel)</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="70"/>
         <source>Start with default KDE settings and data. This option will &lt;b&gt;remove&lt;/b&gt; all data and settings such as contacts, local stored mails, accounts in KMail and Kopete, bookmarks and other such data</source>
-        <translation type="unfinished"></translation>
+        <translation>Start med standard KDE indstillinger og data. Dette valg vil &lt;b&gt;fjerne&lt;/b&gt; al data og alle indstillinger så som kontakter, lokalt gemte emails, konti i KMail og Kopete, bogmærker og anden sådan data</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="73"/>
         <source>Backup existing settings from KDE3 into .kde3-backup. (Highly recommended)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tag sikkerhedskopi af eksisterende indstillinger fra KDE3 til .kde3-backup. (Kraftigt anbefalet)</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="92"/>
         <source>Insufficient free space to complete a backup, please consider freeing up some space. You can go to TTY1 to do this.</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilstrækkelig ledig plads til at fuldføre en sikkerhedskopi, overvej gerne at frigive mere plads. Du kan gå til TTY1 for at gøre dette.</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="96"/>
         <source>The current KDE settings and data directory takes up %1 bytes</source>
-        <translation type="unfinished"></translation>
+        <translation>Nuværende KDE indstillings- og datafolder fylder %1 bytes</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="112"/>
         <source>Recheck</source>
-        <translation type="unfinished"></translation>
+        <translation>Gentjek</translation>
     </message>
 </context>
 <context>
@@ -58,12 +59,12 @@
     <message>
         <location filename="intropage.cpp" line="21"/>
         <source>Kaboom - MigrationTool</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaboom - Migreringsværktøj</translation>
     </message>
     <message>
         <location filename="intropage.cpp" line="22"/>
         <source>This wizard will guide you through the process of migrating your KDE settings as required</source>
-        <translation type="unfinished"></translation>
+        <translation>Denne guide vil hjælpe dig gennem processen med at migrere dine KDE indstillinger som påkrævet</translation>
     </message>
 </context>
 <context>
@@ -71,12 +72,12 @@
     <message>
         <location filename="migrationpage.cpp" line="103"/>
         <source>When you click &quot;start&quot;, settitgs migration will start</source>
-        <translation type="unfinished"></translation>
+        <translation>Når du trykker på &quot;start&quot;, vil migreringen af instillingerne starte</translation>
     </message>
     <message>
         <location filename="migrationpage.cpp" line="106"/>
         <source>Start Migration</source>
-        <translation type="unfinished"></translation>
+        <translation>Start migrering</translation>
     </message>
 </context>
 <context>
@@ -84,17 +85,17 @@
     <message>
         <location filename="migrationpage.cpp" line="30"/>
         <source>Start</source>
-        <translation type="unfinished"></translation>
+        <translation>Start</translation>
     </message>
     <message>
         <location filename="migrationpage.cpp" line="32"/>
         <source>Errors occured</source>
-        <translation type="unfinished"></translation>
+        <translation>Der opstod fejl</translation>
     </message>
     <message>
         <location filename="migrationpage.cpp" line="42"/>
         <source>Migration running</source>
-        <translation type="unfinished"></translation>
+        <translation>Migrering i gang</translation>
     </message>
 </context>
 <context>
@@ -102,17 +103,17 @@
     <message>
         <location filename="migrationtool.cpp" line="37"/>
         <source>&amp;Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Til&amp;bage</translation>
     </message>
     <message>
         <location filename="migrationtool.cpp" line="38"/>
         <source>&amp;Next</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Næste</translation>
     </message>
     <message>
         <location filename="migrationtool.cpp" line="39"/>
         <source>&amp;Finish</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Færdig</translation>
     </message>
 </context>
 <context>
@@ -120,57 +121,57 @@
     <message>
         <location filename="diroperations/diroperations.cpp" line="33"/>
         <source>User canceled the operation</source>
-        <translation type="unfinished"></translation>
+        <translation>Brugeren afbrød handlingen</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="34"/>
         <source>Access was denied to the file or directory %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Adgang nægtet til filen eller mappen %1</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="35"/>
         <source>%1: No such file or directory</source>
-        <translation type="unfinished"></translation>
+        <translation>%1: ingen slig fil eller mappe</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="36"/>
         <source>%1 already exists</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 eksisterer allerede</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="37"/>
         <source>Could not copy %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunne ikke kopiere %1</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="38"/>
         <source>Could not create directory %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunne ikke oprette mappe %1</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="39"/>
         <source>Could not remove %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunne ikke fjerne %1</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="40"/>
         <source>Unknown error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukendt fejl</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="109"/>
         <source>Calculating... %1 bytes</source>
-        <translation type="unfinished"></translation>
+        <translation>Udregner... %1 bytes</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="153"/>
         <source>Copying files...</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopierer filer...</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="260"/>
         <source>Removing files...</source>
-        <translation type="unfinished"></translation>
+        <translation>Fjerner filer...</translation>
     </message>
 </context>
 <context>
@@ -178,12 +179,12 @@
     <message>
         <location filename="warningpage.cpp" line="21"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Advarsel</translation>
     </message>
     <message>
         <location filename="warningpage.cpp" line="22"/>
         <source>You have selected a potentially harmful operation and to not backup data. You will most likely lose data here. Are you completely sure?</source>
-        <translation type="unfinished"></translation>
+        <translation>Du har valgt en potentiel skadelig operation og fravalgt at tage sikkerhedskopier. Du vil sandsynligvis miste data. Er du helt sikker?</translation>
     </message>
 </context>
 </TS>
