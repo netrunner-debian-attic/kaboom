b5c99ac497462fe46cc9a22aab30c3f34e8b02b2
David Palacio <dpalacio@uninorte.edu.co>
2009-02-28 11:15:17 -0500

    Set strings status to finished. Fixed typos.

--- kaboom_es.ts
+++ kaboom_es.ts
@@ -1,56 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="es">
+<defaultcodec></defaultcodec>
 <context>
     <name>ChoicePage</name>
     <message>
         <location filename="choicepage.cpp" line="40"/>
         <source>Kaboom - Migration Tool</source>
-        <translation type="unfinished">Kaboom - Utilidad de migración</translation>
+        <translation>Kaboom - Utilidad de migración</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="44"/>
         <source>Please select your settings migration option</source>
-        <translation type="unfinished">Seleccione la opción de migración de sus configuaciones:</translation>
+        <translation>Seleccione la opción de migración de sus configuraciones</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="49"/>
         <source>Migrate settings from KDE3 to KDE4 (standard)</source>
-        <translation type="unfinished">Migrar las configuraciones de KDE 3 a KDE 4 (estándar)</translation>
+        <translation>Migrar las configuraciones de KDE 3 a KDE 4 (estándar)</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="56"/>
         <source>Use existing KDE 4 settings and &lt;b&gt;replace&lt;/b&gt; KDE 3 settings</source>
-        <translation type="unfinished">Usar las configuraciones actuales de KDE 4 y &lt;b&gt;reemplazar&lt;/b&gt; las configuraciones de KDE 3</translation>
+        <translation>Usar las configuraciones actuales de KDE 4 y &lt;b&gt;reemplazar&lt;/b&gt; las configuraciones de KDE 3</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="61"/>
         <source>Merge settings from KDE3 and KDE4 (experimental)</source>
-        <translation type="unfinished">Fusionar las configuraciones de KDE 3 y KDE 4 (experimental)</translation>
+        <translation>Fusionar las configuraciones de KDE 3 y KDE 4 (experimental)</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="70"/>
         <source>Start with default KDE settings and data. This option will &lt;b&gt;remove&lt;/b&gt; all data and settings such as contacts, local stored mails, accounts in KMail and Kopete, bookmarks and other such data</source>
-        <translation type="unfinished">Empezar con las configuraciones y datos por omisión de KDE . Esta opción &lt;b&gt;eliminará&lt;/b&gt; todos los datos y configuraciones tales como contactos, correos almacenados, cuentas en KMail y Kopete, marcadores y otros datos por el estilo</translation>
+        <translation>Empezar con las configuraciones y datos por omisión de KDE . Esta opción &lt;b&gt;eliminará&lt;/b&gt; todos los datos y configuraciones tales como contactos, correos almacenados, cuentas en KMail y Kopete, marcadores y otros datos por el estilo</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="73"/>
         <source>Backup existing settings from KDE3 into .kde3-backup. (Highly recommended)</source>
-        <translation type="unfinished">Crear una copia de seguridad de la configuración de KDE3 en .kde3-backup. (Opción altamente recomendada)</translation>
+        <translation>Crear una copia de seguridad de la configuración de KDE3 en .kde3-backup. (Altamente recomendado)</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="92"/>
         <source>Insufficient free space to complete a backup, please consider freeing up some space. You can go to TTY1 to do this.</source>
-        <translation type="unfinished">No hay suficiente espacio libre para crear una copia de seguridad, considere liberar algo de espacio en su disco. Puede ir a TTY1 para hacer esto.</translation>
+        <translation>No hay suficiente espacio libre para crear una copia de seguridad, considere liberar algo de espacio en su disco. Puede ir a TTY1 para hacer esto.</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="96"/>
         <source>The current KDE settings and data directory takes up %1 bytes</source>
-        <translation type="unfinished">Los datos y configuraciones actuales de KDE ocupan %1 bytes</translation>
+        <translation>Los datos y configuraciones actuales de KDE ocupan %1 bytes</translation>
     </message>
     <message>
         <location filename="choicepage.cpp" line="112"/>
         <source>Recheck</source>
-        <translation type="unfinished">Recomprobar</translation>
+        <translation>Recomprobar</translation>
     </message>
 </context>
 <context>
@@ -58,12 +59,12 @@
     <message>
         <location filename="intropage.cpp" line="21"/>
         <source>Kaboom - Migration Tool</source>
-        <translation type="unfinished">Kaboom - Utilidad de migración</translation>
+        <translation>Kaboom - Utilidad de migración</translation>
     </message>
     <message>
         <location filename="intropage.cpp" line="22"/>
         <source>This wizard will guide you through the process of migrating your KDE settings as required</source>
-        <translation type="unfinished">Este asistente le guiará a través del proceso de migración de las configuraciones de KDE</translation>
+        <translation>Este asistente le guiará a través del proceso de migración de las configuraciones de KDE</translation>
     </message>
 </context>
 <context>
@@ -71,12 +72,12 @@
     <message>
         <location filename="migrationpage.cpp" line="103"/>
         <source>When you click &quot;Start&quot;, settings migration will start</source>
-        <translation type="unfinished">Cuando seleccione &quot;Comenzar&quot;, se comenzará la migración de configuraciones</translation>
+        <translation>Cuando seleccione &quot;Comenzar&quot;, se comenzará la migración de configuraciones</translation>
     </message>
     <message>
         <location filename="migrationpage.cpp" line="106"/>
         <source>Start Migration</source>
-        <translation type="unfinished">Comenzar migración</translation>
+        <translation>Comenzar migración</translation>
     </message>
 </context>
 <context>
@@ -84,17 +85,17 @@
     <message>
         <location filename="migrationpage.cpp" line="30"/>
         <source>Start</source>
-        <translation type="unfinished">Comenzar</translation>
+        <translation>Comenzar</translation>
     </message>
     <message>
         <location filename="migrationpage.cpp" line="32"/>
         <source>Errors occured</source>
-        <translation type="unfinished">Se encontró un problema</translation>
+        <translation>Se encontró un problema</translation>
     </message>
     <message>
         <location filename="migrationpage.cpp" line="42"/>
         <source>Migration running</source>
-        <translation type="unfinished">Migración ejecutándose</translation>
+        <translation>Ejecutando migración</translation>
     </message>
 </context>
 <context>
@@ -102,17 +103,17 @@
     <message>
         <location filename="migrationtool.cpp" line="37"/>
         <source>&amp;Back</source>
-        <translation type="unfinished">&amp;Anterior</translation>
+        <translation>&amp;Anterior</translation>
     </message>
     <message>
         <location filename="migrationtool.cpp" line="38"/>
         <source>&amp;Next</source>
-        <translation type="unfinished">&amp;Siguiente</translation>
+        <translation>&amp;Siguiente</translation>
     </message>
     <message>
         <location filename="migrationtool.cpp" line="39"/>
         <source>&amp;Finish</source>
-        <translation type="unfinished">&amp;Finalizar</translation>
+        <translation>&amp;Finalizar</translation>
     </message>
 </context>
 <context>
@@ -120,57 +121,57 @@
     <message>
         <location filename="diroperations/diroperations.cpp" line="33"/>
         <source>User canceled the operation</source>
-        <translation type="unfinished">Operación cancelada por el usuario</translation>
+        <translation>Operación cancelada por el usuario</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="34"/>
         <source>Access was denied to the file or directory %1</source>
-        <translation type="unfinished">Le fue denegado el acceso al fichero o directorio %1</translation>
+        <translation>Le fue denegado el acceso al fichero o directorio %1</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="35"/>
         <source>%1: No such file or directory</source>
-        <translation type="unfinished">%1: No existe ese fichero o directorio</translation>
+        <translation>%1: No existe ese fichero o directorio</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="36"/>
         <source>%1 already exists</source>
-        <translation type="unfinished">%1 ya existe</translation>
+        <translation>%1 ya existe</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="37"/>
         <source>Could not copy %1</source>
-        <translation type="unfinished">No se pudo copiar %1</translation>
+        <translation>No se pudo copiar %1</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="38"/>
         <source>Could not create directory %1</source>
-        <translation type="unfinished">No se pudo crear el directorio %1</translation>
+        <translation>No se pudo crear el directorio %1</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="39"/>
         <source>Could not remove %1</source>
-        <translation type="unfinished">No se pudo eliminar %1</translation>
+        <translation>No se pudo eliminar %1</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="40"/>
         <source>Unknown error</source>
-        <translation type="unfinished">Error desconocido</translation>
+        <translation>Error desconocido</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="109"/>
         <source>Calculating... %1 bytes</source>
-        <translation type="unfinished">Calculando... %1 bytes</translation>
+        <translation>Calculando... %1 bytes</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="153"/>
         <source>Copying files...</source>
-        <translation type="unfinished">Copiando ficheros...</translation>
+        <translation>Copiando ficheros...</translation>
     </message>
     <message>
         <location filename="diroperations/diroperations.cpp" line="260"/>
         <source>Removing files...</source>
-        <translation type="unfinished">Eliminando ficheros...</translation>
+        <translation>Eliminando ficheros...</translation>
     </message>
 </context>
 <context>
@@ -178,12 +179,12 @@
     <message>
         <location filename="warningpage.cpp" line="21"/>
         <source>Warning</source>
-        <translation type="unfinished">Atención</translation>
+        <translation>Atención</translation>
     </message>
     <message>
         <location filename="warningpage.cpp" line="22"/>
         <source>You have selected a potentially harmful operation and to not backup data. You will most likely lose data here. Are you completely sure?</source>
-        <translation type="unfinished">Ha seleccionado una operación potencialmente dañina y no crear una copia de seguridad de los datos. Es muy posible que pierda datos. ¿Está completamente seguro?</translation>
+        <translation>Ha seleccionado una operación potencialmente dañina y no crear una copia de seguridad de los datos. Es muy posible que pierda datos. ¿Está completamente seguro?</translation>
     </message>
 </context>
 </TS>
